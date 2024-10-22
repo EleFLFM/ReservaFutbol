4fc5a105d470a9de8c73d918cc67620f262eedaf
EleFLFM <luifermejia1104@gmail.com>
2024-10-21 20:38:16 -0500

    Creacion de modelos y migraciones iniciales

---
+++ database/migrations/2024_10_22_005655_create_reservations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('cancha_id')->constrained();
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->decimal('total_price', 8, 2);
+            $table->enum('status', ['pending', 'confirmed', 'cancelled']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
